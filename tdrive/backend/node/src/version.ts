9d35ecd54a96fbf1219cfd119e7a8b89881e8006
Anton SHEPILOV <ashepilov@linagora.com>
2025-04-23 16:11:27 +0200

    🔖Release 1.0.7-rc10

--- tdrive/backend/node/src/version.ts
+++ tdrive/backend/node/src/version.ts
@@ -1,7 +1,7 @@
 export default {
-  current: /* @VERSION_DETAIL */ "1.0.7-rc9",
+  current: /* @VERSION_DETAIL */ "1.0.7-rc10",
   minimal: {
-    web: /* @MIN_VERSION_WEB */ "1.0.7-rc9",
-    mobile: /* @MIN_VERSION_MOBILE */ "1.0.7-rc9",
+    web: /* @MIN_VERSION_WEB */ "1.0.7-rc10",
+    mobile: /* @MIN_VERSION_MOBILE */ "1.0.7-rc10",
   },
 };
