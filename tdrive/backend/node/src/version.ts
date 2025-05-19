578bd00d3c9b10539ebb5c369b155b555b3e0140
MontaGhanmy <monta.ghanmy@gmail.com>
2025-05-19 10:38:30 +0100

    🔖 Release Version 1.0.7-rc14

--- tdrive/backend/node/src/version.ts
+++ tdrive/backend/node/src/version.ts
@@ -1,7 +1,7 @@
 export default {
-  current: /* @VERSION_DETAIL */ "1.0.7-rc13",
+  current: /* @VERSION_DETAIL */ "1.0.7-rc14",
   minimal: {
-    web: /* @MIN_VERSION_WEB */ "1.0.7-rc13",
-    mobile: /* @MIN_VERSION_MOBILE */ "1.0.7-rc13",
+    web: /* @MIN_VERSION_WEB */ "1.0.7-rc14",
+    mobile: /* @MIN_VERSION_MOBILE */ "1.0.7-rc14",
   },
 };
